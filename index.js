2671687cc9b04fb22928a18cf2701df7dccc24a3
KrishnaKhile <103939308+KrishnaKhile@users.noreply.github.com>
2022-11-30 09:16:41 +0530

    Add link

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const corsOptions=(req,res,next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://krishna-task.netlify.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 }
