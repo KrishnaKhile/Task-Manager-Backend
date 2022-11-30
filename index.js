22a4611ce00a07a3fba528493ffa50e870db03cc
KrishnaKhile <103939308+KrishnaKhile@users.noreply.github.com>
2022-11-30 12:28:03 +0530

    add cors

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const app = express();
 
 // Middleware
 
-const allowedOrigins = ['https://krishna-task.netlify.app/',
-                      'http://localhost:3000/'];
+var allowedOrigins = ['http://localhost:3000',
+                      'https://krishna-task.netlify.app'];
 app.use(cors({
   origin: function(origin, callback){
     // allow requests with no origin 
     // (like mobile apps or curl requests)
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not ' +
+      var msg = 'The CORS policy for this site does not ' +
                 'allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
