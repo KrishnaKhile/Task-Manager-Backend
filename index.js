bb39332fa40b56cfdb881b8b273630648e19a461
KrishnaKhile <103939308+KrishnaKhile@users.noreply.github.com>
2022-11-30 08:55:22 +0530

    add link

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+  "origin":"https://krishna-task.netlify.app"
+}));
 app.use("/api/tasks",router);
 // const logger = (req, res, next) => {
 //   console.log("Middleware run");
