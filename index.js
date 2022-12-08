cba07d67eded7f30b65a6bceb76f1624cf0fb297
KrishnaKhile <103939308+KrishnaKhile@users.noreply.github.com>
2022-12-08 22:48:45 +0530

    cors

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(cors({
   credentials: true,
   origin: 'http://localhost:3000'
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/tasks",router);
