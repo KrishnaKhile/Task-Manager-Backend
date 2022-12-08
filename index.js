d17247235fbcab21839586a7eb195505b9a02dfa
KrishnaKhile <103939308+KrishnaKhile@users.noreply.github.com>
2022-12-08 22:45:30 +0530

    cors

--- index.js
+++ index.js
@@ -8,20 +8,25 @@ const app = express();
 
 // Middleware
 
-var allowedOrigins = ['http://localhost:3000',
-                      'https://preeminent-creponne-75a48c.netlify.app/'];
+// var allowedOrigins = ['http://localhost:3000',
+//                       'https://preeminent-creponne-75a48c.netlify.app'];
+// app.use(cors({
+//   origin: function(origin, callback){
+//     // allow requests with no origin 
+//     // (like mobile apps or curl requests)
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+
 app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+  credentials: true,
+  origin: 'http://localhost:3000'
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
