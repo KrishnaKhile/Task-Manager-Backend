81be9b3b6fbb3659f400ed31d1721c53a7dd6ea5
KrishnaKhile <103939308+KrishnaKhile@users.noreply.github.com>
2022-12-09 08:10:51 +0530

    cors

--- index.js
+++ index.js
@@ -24,7 +24,12 @@ const app = express();
 //   }
 // }));
 
-app.use(cors());
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 // app.use(cors({
 //   credentials: true,
 //   origin: 'http://localhost:3000'
